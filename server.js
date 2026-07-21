4e270329bd27fbd8f4283505a7193b03143dba53
Tyler Kauffman <tyler.kauffman@dealerteam.com>
2026-07-21 16:42:06 -0700

    upodate style

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`Jojo's Pretty Things listening on port ${PORT}`);
 });
